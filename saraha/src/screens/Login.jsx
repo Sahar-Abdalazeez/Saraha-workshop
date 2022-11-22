39074e4b9785f099e80fdd8d68f1ae3a1ed692ef
sahar <saharabdalazeez97@gmail.com>
2022-11-23 00:29:47 +0200

    login screen with navbar image

--- saraha/src/screens/Login.jsx
+++ saraha/src/screens/Login.jsx
@@ -1,6 +1,29 @@
-function Login() {
-    return (
+import React, { useState } from 'react'
 
+import axios from 'axios';
+function Login() {
+    //states 
+    const [user, setUser] = useState({ email: '', password: '' });
+    const [error, setError] = useState({ password: '', email: '' });
+
+    //login function 
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        let { data } = await axios.post('http://localhost:3003/api/v1/auth/signin', user);
+
+        if (data.message === 'validation error') {
+            setError({ password: 'incorrect email or password ', email: 'incorrect email or password ' })
+        }
+        else if (data.message === 'password incorrect') {
+            setError({ password: 'password incorrect' })
+        }
+        else {
+            console.log('hello')
+            //route to home 
+        }
+
+    };
+    return (
         <div className="container text-center my-5">
             <div className="user my-3">
                 <i className="fas fa-user-secret user-icon" />
@@ -8,10 +31,24 @@ function Login() {
             </div>
             <div className="card p-5 w-50 m-auto">
                 <form method="POST" action="/handleLogin">
-                    <input className="form-control" placeholder="Enter your email" type="text" name="email" />
-                    <input className="form-control my-4 " placeholder="Enter your Password" type="text" name="password" />
-                    <button className="btn btn-default-outline my-4 w-100 rounded">Login</button>
+                    <input className="form-control" placeholder="Enter your email" type="text" name="email"
+                        onChange={(email) => {
+                            setError({ email: '' })
+                            setUser((prevUserValue) => ({ ...prevUserValue, email: email.target.value }))
+                        }} />
+                    <div className='text-danger'>{error.email}</div>
+                    <input className="form-control mt-4 " placeholder="Enter your Password" type="text" name="password"
+                        onChange={(password) => {
+                            setError({ password: '', email: '' })
+
+                            setUser((prevUserValue) => ({ ...prevUserValue, password: password.target.value }))
+                        }} />
+                    <div className='text-danger'>{error.password}</div>
+                    <button className="btn btn-default-outline my-4 w-100 rounded" onClick={(e) => {
+                        handleLogin(e);
+                    }} >Login</button>
                     <p><a className="text-muted forgot btn" href>I Forgot My Password</a></p>
+                    {/* route to register  */}
                     <a className="btn btn-default-outline" href="register.html">Register</a>
                 </form>
             </div>
